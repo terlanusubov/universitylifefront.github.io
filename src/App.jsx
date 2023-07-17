b3dcd988809713a07824965de8fba38fa4a7b60d
iboakhmedovv <axmedovibrahim0004@gmail.com>
2023-07-17 20:08:37 +0400

    footer finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
 // Hooks...
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
 
 // Components...
-import { Helmet } from 'react-helmet'
-import { Routes,Route } from 'react-router-dom'
+import { Helmet } from "react-helmet";
+import { Routes, Route } from "react-router-dom";
+import Footer from "./Mutualcomponents/Footer/Footer";
+
 // Pages...
 
 // Stylings...
-import './App.css'
-
-
+import "./App.css";
 
 function App() {
-
-
   return (
-   <div className="App">
-
-
-   </div>
-  )
+    <div className="App">
+      <Footer></Footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
