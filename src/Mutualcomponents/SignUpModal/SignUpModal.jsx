2ecbf07e4f3ddc4a1ebce8af2664c34bda95c59a
KamilAqa <kamil.agayev99@gmail.com>
2023-08-17 15:43:25 +0400

    modal button animation

--- src/Mutualcomponents/SignUpModal/SignUpModal.jsx
+++ src/Mutualcomponents/SignUpModal/SignUpModal.jsx
@@ -40,27 +40,27 @@ function SignUpModal({ open, onClose }) {
   //   localStorage.setItem('token',JSON.stringify(token))
   // }
 
-  const  [nameValue,setNameValue] = useState('')
-  const  [surnameValue,setSurnameValue] = useState('')
-  const  [emailValue,setEmailValue] = useState('')
-  const  [passwordValue,setPasswordValue] = useState('')
+  const [nameValue, setNameValue] = useState('')
+  const [surnameValue, setSurnameValue] = useState('')
+  const [emailValue, setEmailValue] = useState('')
+  const [passwordValue, setPasswordValue] = useState('')
   const [phone, setPhone] = useState('');
 
-  const [loading,setLoading] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   const RegisterHandler = async (e) => {
     e.preventDefault();
     setLoading(true)
-    const filteredPhone = phone.split(" ").join().replaceAll('(', '').replaceAll(')', '').replaceAll(',', '').replaceAll('-','');
+    const filteredPhone = phone.split(" ").join().replaceAll('(', '').replaceAll(')', '').replaceAll(',', '').replaceAll('-', '');
     const registerInformations = {
-      name:nameValue,
-      surname:surnameValue,
-      email:emailValue,
-      phoneNumber:filteredPhone,
-      password:passwordValue,
+      name: nameValue,
+      surname: surnameValue,
+      email: emailValue,
+      phoneNumber: filteredPhone,
+      password: passwordValue,
     }
     console.log(registerInformations);
-    
+
     try {
       const response = await fetch(import.meta.env.VITE_API_KEY, {
         method: 'POST',
@@ -75,8 +75,8 @@ function SignUpModal({ open, onClose }) {
       console.log(description);
       if (description.statusCode === 200) {
         toast.success("Successfully registered.")
-       
-        
+
+
       }
       // console.log(description.description);
       if (description.statusCode === 400) {
@@ -87,7 +87,7 @@ function SignUpModal({ open, onClose }) {
 
     }
     catch (err) {
-     toast.error(err.message);
+      toast.error(err.message);
     }
     setLoading(false)
   }
@@ -134,13 +134,15 @@ function SignUpModal({ open, onClose }) {
           </div>
           <div className='login-title'>
             <h2 className='text-customOrange'>Welcome to University Living</h2>
-              <button className='btn-reg bg-customOrange' onClick={() => toggleTab(1)}>Register</button>
-              <button className='btn-log' onClick={() => toggleTab(2)}>Login</button>
+            <div className='register-login'>
+              <button className={toggle === 1 ? "active" : "btn-reg"} onClick={() => toggleTab(1)}>Register</button>
+              <button className={toggle === 2 ? "active" : "btn-log"} onClick={() => toggleTab(2)}>Login</button>
+            </div>
           </div>
           <form action="/" onSubmit={RegisterHandler} className={toggle === 1 ? "show-content" : "content"} >
 
             <div className="input_container">
-              <input onChange={(e) => setNameValue(e.target.value)}  onFocus={focusHanlder} onBlur={blurHandler} type="name" id='fname' name='fname' className=' border rounded-[10px] p-[10px]' />
+              <input onChange={(e) => setNameValue(e.target.value)} onFocus={focusHanlder} onBlur={blurHandler} type="name" id='fname' name='fname' className=' border rounded-[10px] p-[10px]' />
               <span className='text-white'>Name</span>
             </div>
             <div className="input_container">
@@ -148,11 +150,11 @@ function SignUpModal({ open, onClose }) {
               <span className='text-white'>Surname</span>
             </div>
             <div className="input_container">
-              <input onChange={(e) => setEmailValue(e.target.value)}  onFocus={focusHanlder} onBlur={blurHandler} type="email" id='email' name='email' className=' border rounded-[10px] p-[10px]' />
+              <input onChange={(e) => setEmailValue(e.target.value)} onFocus={focusHanlder} onBlur={blurHandler} type="email" id='email' name='email' className=' border rounded-[10px] p-[10px]' />
               <span className='text-white'>Email</span>
             </div>
             <div className="input_container">
-              <input minLength={8} onChange={(e) => setPasswordValue(e.target.value)}  onFocus={focusHanlder} onBlur={blurHandler} type="password" id='pwd' name='pwd' className=' border rounded-[10px] p-[10px]' />
+              <input minLength={8} onChange={(e) => setPasswordValue(e.target.value)} onFocus={focusHanlder} onBlur={blurHandler} type="password" id='pwd' name='pwd' className=' border rounded-[10px] p-[10px]' />
               <span className='text-white'>Password</span>
             </div>
 
@@ -163,17 +165,17 @@ function SignUpModal({ open, onClose }) {
                 onChange={(phone) => setPhone(phone)}
               />
             </div>
-            <button onSubmit={RegisterHandler} type="submit" className='login bg-customOrange'>{loading ? 
-  <div className="loading_element flex justify-center">
-<div  role="status">
-    <svg   aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-customOrange" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-    </svg>
-    <span class="sr-only">Loading...</span>
-</div>
-  </div>
- : 'Register'}</button>
+            <button onSubmit={RegisterHandler} type="submit" className='login bg-customOrange'>{loading ?
+              <div className="loading_element flex justify-center">
+                <div role="status">
+                  <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-customOrange" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                  </svg>
+                  <span class="sr-only">Loading...</span>
+                </div>
+              </div>
+              : 'Register'}</button>
             <div className='or'>
               <h3 className='text-customOrange'>OR</h3>
             </div>
@@ -189,11 +191,11 @@ function SignUpModal({ open, onClose }) {
 
           <form action="/" className={toggle === 2 ? "show-content" : "content"}>
             <div className="input_container">
-              <input  onFocus={focusHanlder} onBlur={blurHandler} type="email" id='email' name='email' className=' border rounded-[10px] p-[10px]' />
+              <input onFocus={focusHanlder} onBlur={blurHandler} type="email" id='email' name='email' className=' border rounded-[10px] p-[10px]' />
               <span className='text-white'>Email</span>
             </div>
             <div className="input_container">
-              <input  onFocus={focusHanlder} onBlur={blurHandler} type="password" id='pwd' name='pwd' className=' border rounded-[10px] p-[10px]' />
+              <input onFocus={focusHanlder} onBlur={blurHandler} type="password" id='pwd' name='pwd' className=' border rounded-[10px] p-[10px]' />
               <span className='text-white'>Password</span>
             </div>
             <button onSubmit={RegisterHandler} type="submit" className='login bg-customOrange'>Login</button>
@@ -201,7 +203,7 @@ function SignUpModal({ open, onClose }) {
           </form>
 
         </div >
-      </div>
+      </div >
     </>
   );
 }
