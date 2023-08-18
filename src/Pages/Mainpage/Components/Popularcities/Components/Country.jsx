cd364ab814ad363039b05e8834ec1c329fc3fc5a
ElvinSuleymanov <elvinsuleymanov150@gmail.com>
2023-08-18 17:56:30 +0400

    Top Cities connected to api

--- src/Pages/Mainpage/Components/Popularcities/Components/Country.jsx
+++ src/Pages/Mainpage/Components/Popularcities/Components/Country.jsx
@@ -11,9 +11,9 @@ import React from 'react'
 // Assets
 import nybg from '../Assets/newyork.jpg'
 
-const Country = () => {
+const Country = ({bedCount,cityName,id}) => {
   return (
-    <div className='country_element rounded-[10px] cursor-pointer overflow-hidden relative' >
+    <div key={id} className='country_element rounded-[10px] cursor-pointer overflow-hidden relative' >
         <div className="country_element_content rounded-[13px] overflow-hidden">
         <div className="country_element_image rounded-[13px] overflow-hidden">
             <img src={nybg}  className='w-[100%] h-[100%] object-cover rounded-[13px]  hover:scale-[1.3] duration-[0.3s] ease-in'  alt="" />
@@ -22,10 +22,10 @@ const Country = () => {
 
         <div className="country_element_text absolute bottom-[30px] left-[30px] bg-white/20 p-[6px] text-white font-[600] rounded-[10px] backdrop-blur-[10px]">
             <div className="country_name">
-                New York
+                {cityName}
             </div>
             <div className="country_bed_count">
-                1000 Beds
+                {bedCount}
             </div>
         </div>
         
--- src/Pages/Mainpage/Components/Popularcities/Popularcities.jsx
+++ src/Pages/Mainpage/Components/Popularcities/Popularcities.jsx
@@ -1,23 +1,73 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 // Components
-import Countrycontainer from './Components/Countrycontainer'
-
+import Country from './Components/Country'
 // Hooks
 
 // Icons
 
 // Assets
-
+import nybg from './Assets/newyork.jpg'
 
 
 const Popularcities = () => {
+
+  const [cities,setCities] = useState([])
+  
+  
+  const fetchingTopCities = async () => {
+    const firstPromise = await fetch(`${import.meta.env.VITE_API_KEY}/cities?isTop=true`)
+    const firstResponse = await firstPromise.json()
+   
+    console.log(firstResponse);
+    
+      setCities(firstResponse)
+      
+    // const secondPromise = await fetch(`${import.meta.env.VITE_API_KEY}/cities`, {
+    //   method:'POST',
+    //   body:JSON.stringify({
+
+    //   })
+    // })
+  }
+  useEffect(() => {
+    fetchingTopCities()
+  },[])
+
+
   return (
     <div className='main_page_popular_cities px-[50px] max-[1024px]:px-[30px]'>
         <div className="popular_cities_title text-[35px] font-[600] max-[1024px]:text-[32px] max-[800px]:text-[28px]  pb-[20px]">
             Top Cities
         </div>
-        <Countrycontainer></Countrycontainer>
+        <div  className='countries_container grid grid-cols-4 max-[1024px]:grid-cols-2 max-[568px]:grid-cols-1 gap-[13px] gap-x-[20px]'>
+           {
+            cities 
+            &&
+            cities.map((data,index) => {
+              return (
+              <div key={index} className='country_element rounded-[10px] cursor-pointer overflow-hidden relative' >
+                <div className="country_element_content rounded-[13px] overflow-hidden">
+                <div className="country_element_image rounded-[13px] overflow-hidden max-w-[350px] w-[100%] h-[220px] max-[1024px]:max-w-none">
+                    <img src={data.image}  className='w-[100%] h-[100%] object-cover rounded-[13px]  hover:scale-[1.3] duration-[0.3s] ease-in'  alt="" />
+                </div>
+        
+        
+                <div className="flex gap-[10px] flex-col items-start   country_element_text absolute bottom-[30px] left-[30px] bg-white/20 p-[6px] text-white font-[600] rounded-[10px] backdrop-blur-[10px]">
+                    <div className="country_name">
+                        {data.name}
+                    </div>
+                    <div className="country_bed_count">
+                        {data.bedRoomCount} Beds
+                    </div>
+                </div>
+                
+                </div>
+              </div>
+              )
+            })
+           }
+      </div>
     </div>
   )
 }
