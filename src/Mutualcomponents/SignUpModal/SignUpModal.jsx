fda52ecedf655692e260ae0e35c86326f0e1ed83
ElvinSuleymanov <elvinsuleymanov150@gmail.com>
2023-08-19 14:50:51 +0400

    Login Feature Added (Redirection error)

--- src/Mutualcomponents/SignUpModal/SignUpModal.jsx
+++ src/Mutualcomponents/SignUpModal/SignUpModal.jsx
@@ -32,20 +32,47 @@ function SignUpModal({ open, onClose }) {
 
   const [value, setValue] = useState('');
 
-  // const LoginHandler = async (e) => {
-  //   e.preventDefault();
-  //   setLoading(true)
-  //   const loginInformations = {
-  //     email: emailValue,
-  //     password: passwordValue,
-  //   }
-  //   console.log(loginInformations);
-  //   const token = await fetch('api', {
-  //     method: 'POST',
-  //     body: JSON.stringify()
-  //   })
-  //   localStorage.setItem('token', JSON.stringify(token))
-  // }
+
+  const [loginLoading,setLoginLoading] = useState(false)
+
+
+  const [loginEmailValue,setLoginEmailValue] = useState('')
+  const [loginPasswordValue,setLoginPasswordValue] = useState('')
+
+  const LoginHandler = async (e) => {
+    e.preventDefault()
+    setLoginLoading(true)
+
+    const loginInformations = {
+      email: loginEmailValue,
+      password: loginPasswordValue,
+    }
+    console.log(JSON.stringify(loginInformations));
+    const promise = await fetch(`${import.meta.env.VITE_API_KEY}/account/login`, {
+      headers:{
+        'Content-Type':'text/plain'
+      },
+      method:'POST',
+      body: JSON.stringify(loginInformations)
+    })
+   const response = await promise.json()
+   console.log(response);
+    // localStorage.setItem('token', JSON.stringify(token))
+    if (response.statusCode === 200) {
+      toast.success('ugurlu')
+    }
+    else if (response.statusCode === 400) {
+      toast.error('alinmadi')
+    }
+    else if(response.statusCode === 415) {
+      toast.error('Format Duz deyil')
+    } 
+    
+    setLoginLoading(false)
+  }
+
+
+
 
   const [nameValue, setNameValue] = useState('')
   const [surnameValue, setSurnameValue] = useState('')
@@ -69,7 +96,7 @@ function SignUpModal({ open, onClose }) {
     console.log(registerInformations);
 
     try {
-      const response = await fetch(import.meta.env.VITE_API_KEY, {
+      const response = await fetch(`${import.meta.env.VITE_API_KEY}/account/register`, {
         method: 'POST',
         // mode:'no-cors',
         headers: {
@@ -120,7 +147,7 @@ function SignUpModal({ open, onClose }) {
 
   }
 
-
+console.log(loginEmailValue,loginPasswordValue);
 
   const [toggle, setToggle] = useState(1);
 
@@ -146,7 +173,7 @@ function SignUpModal({ open, onClose }) {
               <button className={toggle === 2 ? "active" : "btn-log"} onClick={() => toggleTab(2)}>Login</button>
             </div>
           </div>
-          <form action="/" onSubmit={RegisterHandler} >
+          <form >
 
             <div className="input_container">
               <input onChange={(e) => setNameValue(e.target.value)} onFocus={focusHanlder} onBlur={blurHandler} type="name" id='fname' name='fname' className=' border rounded-[10px] p-[10px]' />
@@ -172,7 +199,7 @@ function SignUpModal({ open, onClose }) {
                 onChange={(phone) => setPhone(phone)}
               />
             </div>
-            <button onSubmit={RegisterHandler} type="submit" className='login bg-customOrange'>{loading ?
+            <button onClick={RegisterHandler} className='login bg-customOrange'>{loading ?
               <div className="loading_element flex justify-center">
                 <div role="status">
                   <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-customOrange" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -212,17 +239,26 @@ function SignUpModal({ open, onClose }) {
               <button className={toggle === 2 ? "active" : "btn-log"} onClick={() => toggleTab(2)}>Login</button>
             </div>
           </div>
-          <form action="/" >
+          <form >
             <div className="input_container">
-              <input onFocus={focusHanlder} onBlur={blurHandler} type="email" id='email' name='email' className=' border rounded-[10px] p-[10px]' />
+              <input value={loginEmailValue} onChange={(e) => setLoginEmailValue(e.target.value)} onFocus={focusHanlder} onBlur={blurHandler} type="email" id='loginEmail' name='email' className=' border rounded-[10px] p-[10px]' />
               <span className='text-white'>Email</span>
             </div>
             <div className="input_container">
-              <input onFocus={focusHanlder} onBlur={blurHandler} type="password" id='pwd' name='pwd' className=' border rounded-[10px] p-[10px]' />
+              <input value={loginPasswordValue} onChange={(e) => setLoginPasswordValue(e.target.value)} onFocus={focusHanlder} onBlur={blurHandler} type="password" id='loginPwd' name='pwd' className=' border rounded-[10px] p-[10px]' />
               <span className='text-white'>Password</span>
             </div>
-            <button onSubmit={RegisterHandler} type="submit" className='login bg-customOrange'>Login</button>
-
+            <button onClick={LoginHandler} className='login bg-customOrange'>{loginLoading ?
+              <div className="loading_element flex justify-center">
+                <div role="status">
+                  <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-customOrange" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                  </svg>
+                  <span class="sr-only">Loading...</span>
+                </div>
+              </div>
+              : 'Login'}</button>
           </form>
         </div >
       </div >
