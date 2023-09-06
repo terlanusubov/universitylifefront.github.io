5f3381633b5a41306557ce1620d7b975bf1f0a26
ibrahim <axmedovibrahim0004@gmail.com>
2023-09-06 21:47:26 +0400

    contact error

--- src/Pages/Contact/Components/Contactform/Components/ContactForm.jsx
+++ src/Pages/Contact/Components/Contactform/Components/ContactForm.jsx
@@ -1,9 +1,8 @@
-import React, { useRef, useState,useEffect } from "react";
-// import { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { FaLocationDot } from "react-icons/fa6";
 import ContactInput from "../Components/ContactInput";
 import ContactInputComponent from "./ContactInputComponent";
-import { toast,Toaster } from "react-hot-toast";
+import { toast, Toaster } from "react-hot-toast";
 import "../../../Styles/ContactInForm.css";
 export const ContactForm = () => {
   // Toggles
@@ -11,53 +10,77 @@ export const ContactForm = () => {
   const [popUpOpened, setPopUpOpened] = useState(false);
   // Input Values
   const popupInputRef = useRef();
-  const [phoneInputValue,setPhoneInputValue] = useState('');
+  const [phoneInputValue, setPhoneInputValue] = useState("");
   const [labelGone, setLabelGone] = useState(false);
   const [inputCountryValue, setInputCountryValue] = useState("");
   const [contactDescription, setContactDescription] = useState("");
   // Input Values
+  const [countries, setCountries] = useState([]);
+  const [isFullNameFocused, setIsFullNameFocused] = useState(false);
+  const [isEmailFocused, setIsEmailFocused] = useState(false);
+  //////
+  const [selectedCountry, setSelectedCountry] = useState("");
+  //////
+  const [filteredCountries, setFilteredCountries] = useState([]);
 
-const [countries,setCountries] = useState([]);
+  //
+  const submitFormHandler = async (e) => {
+    e.preventDefault();
 
+    const filteredPhone = phoneInputValue
+      .replaceAll("-", "")
+      .replaceAll(" ", "")
+      .replaceAll("(", "")
+      .replaceAll(")", "");
 
-const submitFormHandler = async (e) => {
-e.preventDefault()
+    const formInfos = {
+      fullName: ContactFullName,
+      email: ContactEmail,
+      phone: filteredPhone,
+      // country: inputCountryValue,
+      ///////
+      country: selectedCountry,
+      //////
+      comment: contactDescription,
+    };
 
-const filteredPhone = phoneInputValue.replaceAll('-','').replaceAll(' ','').replaceAll('(','').replaceAll(')','')
+    if (
+      ContactFullName == "" ||
+      ContactEmail == "" ||
+      filteredPhone == "" ||
+      /////
+      selectedCountry == "" ||
+      /////
+      // inputCountryValue == "" ||
+      contactDescription == ""
+    ) {
+      toast.error("Please Fill All Blanks");
+      return false;
+    }
 
-const formInfos = {
-  fullName:ContactFullName,
-  email:ContactEmail,
-  phone:filteredPhone,
-  country:inputCountryValue,
-  comment:contactDescription
-}
+    const response = await fetch(import.meta.env.VITE_API_KEY + "Contact", {
+      headers: {
+        "Content-type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(formInfos),
+    });
+    const result = await response.json();
+    if (result.statusCode === 200) {
+      toast.success("Form Successfully Sent");
+    } else {
+      toast.error("Something Unknown Happened");
+    }
 
-const response = await fetch(import.meta.env.VITE_API_KEY + 'Contact', {
-  headers: {
-    'Content-type':'application/json'
-  },
-  method:'POST',
-  body: JSON.stringify(formInfos)
-
-
-})
-
-const result = await response.json()
-if (result.statusCode === 200) {
-toast.success("Form Successfully Sent")
-}
-else {
-  toast.error("Something Unknown Happened")
-}
-
-setContactDescription('')
-setContactEmail('')
-setContactFullName('')
-setPhoneInputValue('')
-setInputCountryValue('')
-}
-
+    setContactDescription("");
+    setContactEmail("");
+    setContactFullName("");
+    setPhoneInputValue("");
+    setInputCountryValue("");
+    setDescriptionFocused(false);
+    setIsEmailFocused(false);
+    setIsFullNameFocused(false);
+  };
 
   const focusHandler = (e) => {
     setPopUpOpened(true);
@@ -75,7 +98,7 @@ setInputCountryValue('')
   };
 
   const blurHandlerCountry = (e) => {
-    if (inputCountryValue == '') {
+    if (inputCountryValue == "") {
       setLabelGone(false);
     }
   };
@@ -83,166 +106,213 @@ setInputCountryValue('')
   document.addEventListener("click", (e) => {
     try {
       if (!popupInputRef.current.contains(e.target)) {
-          setPopUpOpened(false);
+        setPopUpOpened(false);
         // setLabelGone(false);
-        
       }
-    } catch (e) {
-    }
+    } catch (e) {}
   });
+  //////
+  const handleCountryInputChange = (e) => {
+    const value = e.target.value;
+    setInputCountryValue(value);
 
-  const setCurrentTravelCountry = (e,value) => {
-    setInputCountryValue(value)
-    setPopUpOpened(false)
+    const filtered = countries.filter((country) =>
+      country.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredCountries(filtered);
+    // setCountries(filtered);
   };
-
- 
-
-  // const inputLabelHandler = () => {
-  //   if (inputCountryValue) {
-  //     setLabelGone(false);
-  //   }
-  // };
-
-
-
-  // silme
-
-  // .filter((count) => inputCountryValue.includes(count.name))
-
-
+  //////
+  const setCurrentTravelCountry = (e, value) => {
+    //////
+    setInputCountryValue(value);
+    setSelectedCountry(value);
+    setPopUpOpened(false);
+    //////
+  };
 
   const [ContactFullName, setContactFullName] = useState("");
   const [ContactEmail, setContactEmail] = useState("");
   const [ContactTravveling, setContactTravveling] = useState("");
 
-
-useEffect(() => {
-fetch(import.meta.env.VITE_API_KEY + `countries`).then(data => data.json()).then((data) => setCountries(data));
-},[])
-
+  useEffect(() => {
+    fetch(import.meta.env.VITE_API_KEY + `countries`)
+      .then((data) => data.json())
+      .then((data) => setCountries(data));
+  }, []);
   return (
     <>
-    <Toaster/>
-    <form action="">
-      <div className="grid md:grid-cols-2 md:gap-4 ">
-        <div className="mb-4">
-          <ContactInputComponent
-            setInputValue={setContactFullName}
-            inputId={"ContactFullName"}
-            inputTitle={"Full Name"}
-            inputValue={ContactFullName}
-            autoComplete={false}
+      <Toaster />
+      <form action="">
+        <div className="grid md:grid-cols-2 md:gap-4 ">
+          <div className="mb-4">
+            <ContactInputComponent
+              setInputValue={setContactFullName}
+              inputId={"ContactFullName"}
+              inputTitle={"Full Name"}
+              inputValue={ContactFullName}
+              isFocused={isFullNameFocused}
+              setIsFocused={setIsFullNameFocused}
             />
-        </div>
-        <div className="mb-4">
-          <ContactInputComponent
-            setInputValue={setContactEmail}
-            inputId={"ContactEmail"}
-            inputTitle={"Email"}
-            inputValue={ContactEmail}
-            autoComplete={false}
+          </div>
+          <div className="mb-4">
+            <ContactInputComponent
+              setInputValue={setContactEmail}
+              inputId={"ContactEmail"}
+              inputTitle={"Email"}
+              inputValue={ContactEmail}
+              isFocused={isEmailFocused}
+              setIsFocused={setIsEmailFocused}
             />
-        </div>
-        <div className="mb-4">
-          <ContactInput setInputValue={setPhoneInputValue} inputValue={phoneInputValue}/>
-        </div>
-        <div className="mb-4 relative z-[20]" ref={popupInputRef}>
-          <input
-            onBlur={blurHandlerCountry}
-            onFocus={focusHandler}
-            // onBlur={inputLabelHandler}
-            className="border outline-none w-[100%] rounded-[4px]  p-[9px]"
-            id="traveled_country"
-            type="text"
-            name="traveled_country"
-            autoComplete={false}
-            value={inputCountryValue}
-            onChange={(e) => setInputCountryValue(e.target.value)}
+          </div>
+          <div className="mb-4">
+            <ContactInput
+              setInputValue={setPhoneInputValue}
+              inputValue={phoneInputValue}
             />
-          <label
-            className={`duration-[.3s] bg-white px-[3px] text-gray-700  absolute
+          </div>
+
+          {/* <div className="mb-4 relative z-[20]" ref={popupInputRef}>
+            <input
+              onBlur={blurHandlerCountry}
+              onFocus={focusHandler}
+              className="border outline-none w-[100%] rounded-[4px]  p-[9px]"
+              id="traveled_country"
+              type="text"
+              name="traveled_country"
+              autoComplete="new-password"
+              value={inputCountryValue}
+              // onChange={(e) => setInputCountryValue(e.target.value)}
+              //////
+              onChange={handleCountryInputChange}
+              //////
+            />
+            <label
+              className={`duration-[.3s] bg-white px-[3px] text-gray-700  absolute
             ${
-              inputCountryValue ? "top-[-10px] left-[16px] text-[11px]" 
-              :
-              labelGone
-              ? "top-[-10px] left-[16px] text-[11px]"
-              :   "top-[10px] left-[16px] text-[14px]"
+              inputCountryValue
+                ? "top-[-10px] left-[16px] text-[11px]"
+                : labelGone
+                ? "top-[-10px] left-[16px] text-[11px]"
+                : "top-[10px] left-[16px] text-[14px]"
             }
             `}
-            htmlFor="traveled_country"
+              htmlFor="traveled_country"
             >
-            Travelling Country<sup className="text-customOrange">*</sup>
-          </label>
-          {popUpOpened ? (
-            <div className="popup  w-[100%] bg-white border-solid border-[1px]  rounded-b h-[100px] absolute overflow-y-scroll">
-
-        
-
-              {
-                countries.length
-                && 
-                countries.map(((country) => {
-                  
-                  return (
-                    <div key={country.id}
-                onClick={(e) => setCurrentTravelCountry(e,country.name)} 
-                data-value={country.name}
-                className="z-[20] flex items-center p-2.5 cursor-pointer"
-                >
-                <span className="text-customOrange mr-1.5">
-                  <FaLocationDot></FaLocationDot>
-                </span>{" "}
-                {country.name}
+              Travelling Country<sup className="text-customOrange">*</sup>
+            </label>
+            {popUpOpened ? (
+              <div className="popup  w-[100%] bg-white border-solid border-[1px]  rounded-b h-[100px] absolute overflow-y-scroll">
+                {countries.length &&
+                  countries.map((country) => {
+                    return (
+                      <div
+                        key={country.id}
+                        onClick={(e) =>
+                          setCurrentTravelCountry(e, country.name)
+                        }
+                        data-value={country.name}
+                        className="z-[20] flex items-center p-2.5 cursor-pointer"
+                      >
+                        <span className="text-customOrange mr-1.5">
+                          <FaLocationDot></FaLocationDot>
+                        </span>{" "}
+                        {country.name}
+                      </div>
+                    );
+                  })}
               </div>
-                )
-                
-              }))
-            }
-
-
-           
-            </div>
-          ) : (
-            ""
+            ) : (
+              ""
             )}
+          </div> */}
+          <div className="mb-4 relative z-[20]" ref={popupInputRef}>
+            <input
+              onBlur={blurHandlerCountry}
+              onFocus={focusHandler}
+              className="border outline-none w-[100%] rounded-[4px] p-[9px]"
+              id="traveled_country"
+              type="text"
+              name="traveled_country"
+              autoComplete="new-password"
+              value={inputCountryValue}
+              onChange={handleCountryInputChange}
+            />
+            <label
+              className={`duration-[.3s] bg-white px-[3px] text-gray-700  absolute
+              ${
+                inputCountryValue
+                  ? "top-[-10px] left-[16px] text-[11px]"
+                  : labelGone
+                  ? "top-[-10px] left-[16px] text-[11px]"
+                  : "top-[10px] left-[16px] text-[14px]"
+              }
+            `}
+              htmlFor="traveled_country"
+            >
+              Travelling Country<sup className="text-customOrange">*</sup>
+            </label>
+            {popUpOpened ? (
+              <div className="popup  w-[100%] bg-white border-solid border-[1px]  rounded-b h-[100px] absolute overflow-y-scroll">
+                {filteredCountries.length > 0 ? (
+                  filteredCountries.map((country) => (
+                    <div
+                      key={country.id}
+                      onClick={(e) => setCurrentTravelCountry(e, country.name)}
+                      data-value={country.name}
+                      className="z-[20] flex items-center p-2.5 cursor-pointer"
+                    >
+                      <span className="text-customOrange mr-1.5">
+                        <FaLocationDot></FaLocationDot>
+                      </span>{" "}
+                      {country.name}
+                    </div>
+                  ))
+                ) : (
+                  <div className="p-2.5">No matching countries found.</div>
+                )}
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
-      </div>
-      <div className="text-area-input mb-4 relative">
-        <textarea
-          id="textAreaContact"
-          value={contactDescription}
-          onChange={(e) => setContactDescription(e.target.value)}
-          onFocus={focusHandlerTextArea}
-          onBlur={blurHandlerTextArea}
-          className="border outline-none w-[100%] rounded-[4px]  p-[9px]"
-          placeholder=""
-          cols="30"
-          rows="5"
-          autoComplete={false}
+        <div className="text-area-input mb-4 relative">
+          <textarea
+            id="textAreaContact"
+            value={contactDescription}
+            onChange={(e) => setContactDescription(e.target.value)}
+            onFocus={focusHandlerTextArea}
+            onBlur={blurHandlerTextArea}
+            className="border outline-none w-[100%] rounded-[4px]  p-[9px]"
+            placeholder=""
+            cols="30"
+            rows="5"
+            // autoComplete="off"
+            autoComplete="new-password"
           ></textarea>
-        <label
-          htmlFor="textAreaContact"
-          className={` select-none input_text absolute ease-in-out text-gray-500 
+          <label
+            htmlFor="textAreaContact"
+            className={` select-none input_text absolute ease-in-out text-gray-500 
           text-gray-500 rounded-[2px]  bg-white mb-2 absolute duration-[.2s] ${
             descriptionFocused
               ? "top-[-9px] left-[16px] text-[11px] px-[0.5rem]"
               : "top-[10px]  left-[16px] text-[14px] "
           }`}
           >
-          Comments/Description <sup className="text-customOrange">*</sup>
-        </label>
-      </div>
-      <div className="w-full flex justify-center">
-        <button
-        onClick={submitFormHandler}
-        className="w-full flex justify-center lg:w-auto lg:mx-auto my-5 te-cu-submit  my-1 px-16 py-2 rounded-md text-base bg-customOrange text-white hover:bg-theme-orange-hover border border-theme-orange transition-colors disabled:opacity-70 disabled:cursor-not-allowed"
-        type="Submit"
-        >
-          Submit
-        </button>
-      </div>
-    </form>
-          </>
+            Comments/Description <sup className="text-customOrange">*</sup>
+          </label>
+        </div>
+        <div className="w-full flex justify-center">
+          <button
+            onClick={submitFormHandler}
+            className="w-full flex justify-center lg:w-auto lg:mx-auto my-5 te-cu-submit  my-1 px-16 py-2 rounded-md text-base bg-customOrange text-white hover:bg-theme-orange-hover border border-theme-orange transition-colors disabled:opacity-70 disabled:cursor-not-allowed"
+            type="Submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
--- src/Pages/Contact/Components/Contactform/Components/ContactInputComponent.jsx
+++ src/Pages/Contact/Components/Contactform/Components/ContactInputComponent.jsx
@@ -5,38 +5,43 @@ const ContactInputComponent = ({
   inputId,
   inputValue,
   setInputValue,
+  isFocused,
+  setIsFocused,
 }) => {
-  const [focused, setFocused] = useState(false);
+  // const [focused, setFocused] = useState(false);
+
   const focusHandler = () => {
-    setFocused(true);
+    setIsFocused(true);
   };
   const blurHandler = () => {
     if (!inputValue) {
-      setFocused(false);
+      setIsFocused(false);
     }
   };
 
   return (
     <div className="input_element_container relative w-[100%]">
-        <input
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          id={inputId}
-          onBlur={blurHandler}
-          onFocus={focusHandler}
-          type={
-            inputTitle === "Password"
-              ? "password"
-              : inputTitle === "Email"
-              ? "email"
-              : "text"
-          }
-          className="border outline-none w-[100%] rounded-[4px]  p-[9px]"
-        />
+      <input
+        // autoComplete="off"
+        autoComplete="new-password"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        id={inputId}
+        onBlur={blurHandler}
+        onFocus={focusHandler}
+        type={
+          inputTitle === "Password"
+            ? "password"
+            : inputTitle === "Email"
+            ? "email"
+            : "text"
+        }
+        className="border outline-none w-[100%] rounded-[4px]  p-[9px]"
+      />
       <label
         htmlFor={inputId}
         className={`select-none input_text absolute  rounded-[2px] bg-white translate-y-[-50%] ease-in-out text-gray-500 duration-[.25s] px-[4px]  left-[15px] ${
-          focused ? "top-0 text-[11px]" : "top-[50%] text-[14px]"
+          isFocused ? "top-0 text-[11px]" : "top-[50%] text-[14px]"
         }`}
       >
         {inputTitle}
