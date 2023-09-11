1390c84cce8b9fee7d295378b5d0bb94d933c35b
Farida565a3 <feride565a3@gmail.com>
2023-09-11 15:30:10 +0400

    c

--- src/Pages/Accommodation/Components/Card.jsx
+++ src/Pages/Accommodation/Components/Card.jsx
@@ -2,25 +2,27 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import addimg from '../Assets/img/add.jpg'
 import offerimg from '../Assets/img/offer.jpg'
-import img from '../Assets/img/img2.jpg'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import { useCart } from 'react-use-cart';
 
-const Card = () => {
+const Card = (props) => {
+    const {addItem} = useCart();
+
     return (
-        <div className='card'>
+        <div className='card' >
             <div className='rounded-lg shadow-md hover:shadow-md transition-all flex-shrink-0 lg:w-full flex flex-col text-opacity-100 text-[#3e3E3E]'>
                 <div className='relative h-52'>
                     <Link className='w-full h-full'>
                         <Carousel infiniteLoop={true} showThumbs={false} showStatus={false}>
                             <div>
-                                <img className='h-52 w-full rounded-t-md'  src={img} />
+                                <img className='h-52 w-full rounded-t-md'  src={props.img} />
                             </div>
                             <div>
-                                <img className='h-52 w-full rounded-t-md'  src={img} />
+                                <img className='h-52 w-full rounded-t-md'  src={props.img} />
                             </div>
                             <div>
-                                <img className='h-52 w-full rounded-t-md'  src={img} />
+                                <img className='h-52 w-full rounded-t-md'  src={props.img} />
                             </div>
                         </Carousel>
                     </Link>
@@ -34,7 +36,7 @@ const Card = () => {
                         <svg className="w-4 h-4 mr-1 lg:hidden xl:block motion-safe:animate-bounce" fill="currentColor" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M8 8c.828 0 1.5-.895 1.5-2S8.828 4 8 4s-1.5.895-1.5 2S7.172 8 8 8Z"></path><path d="M11.953 8.81c-.195-3.388-.968-5.507-1.777-6.819C9.707 1.233 9.23.751 8.857.454a3.495 3.495 0 0 0-.463-.315A2.19 2.19 0 0 0 8.25.064.546.546 0 0 0 8 0a.549.549 0 0 0-.266.073 2.312 2.312 0 0 0-.142.08 3.67 3.67 0 0 0-.459.33c-.37.308-.844.803-1.31 1.57-.805 1.322-1.577 3.433-1.774 6.756l-1.497 1.826-.004.005A2.5 2.5 0 0 0 2 12.202V15.5a.5.5 0 0 0 .9.3l1.125-1.5c.166-.222.42-.4.752-.57.214-.108.414-.192.625-.281l.198-.084c.7.428 1.55.635 2.4.635.85 0 1.7-.207 2.4-.635.067.03.132.056.196.083.213.09.413.174.627.282.332.17.586.348.752.57l1.125 1.5a.5.5 0 0 0 .9-.3v-3.298a2.5 2.5 0 0 0-.548-1.562l-1.499-1.83ZM12 10.445v.055c0 .866-.284 1.585-.75 2.14.146.064.292.13.425.199.39.197.8.46 1.1.86L13 14v-1.798a1.5 1.5 0 0 0-.327-.935L12 10.445ZM4.75 12.64C4.284 12.085 4 11.366 4 10.5v-.054l-.673.82a1.5 1.5 0 0 0-.327.936V14l.225-.3c.3-.4.71-.664 1.1-.861.133-.068.279-.135.425-.199ZM8.009 1.073c.063.04.14.094.226.163.284.226.683.621 1.09 1.28C10.137 3.836 11 6.237 11 10.5c0 .858-.374 1.48-.943 1.893C9.517 12.786 8.781 13 8 13c-.781 0-1.517-.214-2.057-.607C5.373 11.979 5 11.358 5 10.5c0-4.182.86-6.586 1.677-7.928.409-.67.81-1.082 1.096-1.32.09-.076.17-.135.236-.18Z"></path><path d="M9.479 14.361c-.48.093-.98.139-1.479.139-.5 0-.999-.046-1.479-.139L7.6 15.8a.5.5 0 0 0 .8 0l1.079-1.439Z"></path></svg>
                         <span className='pl-1 text-[12px] lg:text-[10px] xl:text-[12px] font-semibold leading-4'>Filling Fast</span>
                     </div>
-                    <div className='cursor-pointer flex items-center bottom-2 right-2 rounded p-2 absolute text-white bg-[#00000099]'>
+                    <div onClick={()=>addItem(props.item)} className='cursor-pointer flex items-center bottom-2 right-2 rounded p-2 absolute text-white bg-[#00000099]'>
                         <img className='w-[15px] h-[15px]' src={addimg} alt="" />
                         <span className='pl-2 text-xs font-semibold'>Add to Compare</span>
                     </div>
@@ -45,21 +47,21 @@ const Card = () => {
                 <div className='w-full bg-white rounded-b-md p-3'>
                     <div className='flex mb-2 justify-between items-center'>
                         <Link className='font-bold max-w-[73%] truncate text-[#3E3E3E]'>
-                            <p className='truncate'>Wick Park Residence</p>
+                            <p className='truncate'>{props.title}</p>
                         </Link>
-                        <div className='max-w-[192px] truncate text-[#3E3E3E]'>4.91 miles</div>
+                        <div className='max-w-[192px] truncate text-[#3E3E3E]'>{props.distance} miles</div>
                     </div>
                     <div className='flex justify-between items-center my-1.5'>
                         <div className='text-xs truncate pr-4'>
-                            Studio &nbsp; · &nbsp; Ensuite &nbsp; &nbsp;
+                            {props.type}&nbsp; 
                         </div>
                         <div className='text-right text-xs flex-shrink-0'>Starting From</div>
                     </div>
                     <div className='flex justify-between items-end space-x-6'>
                         <div className='text-xs flex items-center w-[60%] whitespace-nowrap'>
-                            <span className='truncate'>London</span>
+                            <span className='truncate'>{props.city}</span>
                         </div>
-                        <div className='text-right text-customOrange font-bold text-[1rem] leading-5'>437 &nbsp; £/week</div>
+                        <div className='text-right text-customOrange font-bold text-[1rem] leading-5'>{props.price} &nbsp; £/week</div>
                     </div>
                     <div className='flex items-end justify-between mt-1.5'>
                         <div className='mt-1 flex'>
--- src/Pages/Accommodation/Components/Cards.jsx
+++ src/Pages/Accommodation/Components/Cards.jsx
@@ -1,33 +1,47 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Card from './Card'
-import { slides } from './Caruseldata.json'
+import Caruseldata from './Caruseldata'
 import ReactPaginate from 'react-paginate';
 import '../Styles/cards.css'
 
 const Cards = () => {
+  const [items,setItems]=useState(Caruseldata.productData);
+  const[pageNumber,setPageNumber]=useState(0);
+  const itemPerPage=12;
+  const pageVisited=pageNumber*itemPerPage;
+  const displayItems=items.slice(pageVisited,pageVisited+itemPerPage).map((item,index)=>{
+    return(
+      <Card 
+             title={item.title}
+             city={item.city}
+              img={item.img}
+              price={item.price}
+              distance={item.distance}
+              type={item.type}
+              item={item}
+              key={index}
+              />
+    
+    )
+  });
+  const pageCount = Math.ceil(items.lenght/itemPerPage);
+  const changepage = ({selected})=>{
+    setPageNumber(selected)
+  }
+  const itemslenght = items.length
   return (
     <div>
       <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-3 mb-7">
-        <Card data={slides} />
-        <Card data={slides} />
-        <Card data={slides} />
-        <Card data={slides} />
-        <Card data={slides} />
-        <Card data={slides} />
-        <Card data={slides} />
-        <Card data={slides} />
-        <Card data={slides} />
-        <Card data={slides} />
-        <Card data={slides} />
-        <Card data={slides} />
+        {displayItems}
+       
       </div>
       <div className='sm:flex sm:flex-1 sm:items-center sm:justify-between'>
         <div className='hidden lg:block'>
           <p className='text-gray-700 text-sm'>
             Showing
-            <span className='font-medium'>&nbsp;1&nbsp;-&nbsp;12&nbsp;</span>
+            <span className='font-medium'>&nbsp;1&nbsp;-&nbsp;{pageCount} &nbsp;</span>
             properties out of
-            <span className='font-medium'>&nbsp;203</span>
+            <span className='font-medium'>&nbsp;{itemslenght}</span>
           </p>
         </div>
         <div className='flex items-center'>
@@ -35,7 +49,8 @@ const Cards = () => {
           <ReactPaginate
             previousLabel={'<'}
             nextLabel={'>'}
-            pageCount={17}
+            pageCount={pageCount}
+            onPageChange={changepage}
             pageRangeDisplayed={3}
             marginPagesDisplayed={1}
             containerClassName={'paginate'}
