ef7ded5f567b05c6b338f4bdd41f54b98e2501d9
ibrahim <axmedovibrahim0004@gmail.com>
2023-09-12 19:04:41 +0400

    Slider added

--- src/Pages/DetailPage/Components/DetailpagePhoto/Components/DetailPhotoCollage.jsx
+++ src/Pages/DetailPage/Components/DetailpagePhoto/Components/DetailPhotoCollage.jsx
@@ -3,6 +3,9 @@ import { FaImage } from "react-icons/fa6";
 /////////
 import "../../../Styles/DetailPhotoCollage.css";
 /////////
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from "react-responsive-carousel";
+
 const DetailPhotoCollage = () => {
   return (
     <div className="md:my-5 w-full px-0 sm:px-5">
@@ -10,18 +13,35 @@ const DetailPhotoCollage = () => {
         <div className="bg-[#f1f1f1] relative md:h-[30vh] lg:h-[45vh] h-[24vh] max-h-[220px] md:max-h-[none]">
           <div>
             <div>
-              <img
-                alt="Nottingham Two Nottingham 0"
-                sizes="(max-width: 768px) 50vw, 40vw"
-                srcset="https://cdn.universityliving.com/fit-in/256x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg 256w, https://cdn.universityliving.com/fit-in/384x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg 384w, https://cdn.universityliving.com/fit-in/640x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg 640w, https://cdn.universityliving.com/fit-in/750x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg 750w, https://cdn.universityliving.com/fit-in/828x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg 828w, https://cdn.universityliving.com/fit-in/1080x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg 1080w, https://cdn.universityliving.com/fit-in/1200x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg 1200w, https://cdn.universityliving.com/fit-in/1920x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg 1920w"
-                src="https://cdn.universityliving.com/fit-in/1920x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg"
-                width="640"
-                height="360"
-                decoding="async"
-                data-nimg="1"
-                class="te-property-img w-full object-cover cursor-pointer md:h-[45vh] h-[24vh] md:rounded-l-xl max-h-[220px] md:max-h-[none] object-top"
-              />
             </div>
+
+              <Carousel showIndicators={true} dynamicHeight={true} className="w-[100%] h-[400px] relative" infiniteLoop={true} showArrows={true}  showThumbs={false} >
+                <div className="item w-[100%] max-w-[640px] h-[330px] object-cover">
+               <img className="w-[100%] object-cover " src={"https://cdn.universityliving.com/fit-in/1920x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg"} alt="" />
+                </div>
+
+                <div className="item w-[100%] max-w-[640px] h-[330px] object-cover">
+               <img className="w-[100%] object-cover " src={"https://cdn.universityliving.com/fit-in/1920x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg"} alt="" />
+                </div>
+                <div className="item w-[100%] max-w-[640px] h-[330px] object-cover">
+               <img className="w-[100%] object-cover " src={"https://cdn.universityliving.com/fit-in/1920x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg"} alt="" />
+                </div>
+
+              </Carousel>
+
+            {/* <div>
+                  <img
+                    alt="Nottingham Two Nottingham 0"
+                    sizes="(max-width: 768px) 50vw, 40vw"
+                    srcset="https://cdn.universityliving.com/fit-in/256x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg 256w, https://cdn.universityliving.com/fit-in/384x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg 384w, https://cdn.universityliving.com/fit-in/640x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg 640w, https://cdn.universityliving.com/fit-in/750x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg 750w, https://cdn.universityliving.com/fit-in/828x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg 828w, https://cdn.universityliving.com/fit-in/1080x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg 1080w, https://cdn.universityliving.com/fit-in/1200x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg 1200w, https://cdn.universityliving.com/fit-in/1920x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg 1920w"
+                    src="https://cdn.universityliving.com/fit-in/1920x0/filters:no_upscale()/filters:format(webp)/cms/TV2uZnmzr73Wa9VvtCK8t9mGqQ5sSV.jpg"
+                    width="640"
+                    height="360"
+                    decoding="async"
+                    data-nimg="1"
+                    class="te-property-img w-full object-cover cursor-pointer md:h-[45vh] h-[24vh] md:rounded-l-xl max-h-[220px] md:max-h-[none] object-top"
+                  />
+                </div> */}
           </div>
           <div className="absolute bottom-2 flex justify-between z-[2] w-full md:w-auto px-2 md:bottom-4 md:px-2.5">
             <div className="block"></div>
