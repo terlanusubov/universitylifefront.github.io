bd601fe09ce420223d1d8a06a584afab79e8e212
ElvinSuleymanov <elvinsuleymanov150@gmail.com>
2023-09-27 22:30:06 +0400

    autoFocus in name input modal (for ios )

--- src/Mutualcomponents/SignModal/Components/InputComponent.jsx
+++ src/Mutualcomponents/SignModal/Components/InputComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useState,useRef,useEffect} from 'react'
 const InputComponent = ({inputTitle,inputId,inputValue,setInputValue}) => {
+
     const [focused,setFocused] = useState(false)
     const focusHandler = () => {
         setFocused(true)
@@ -11,13 +12,17 @@ const InputComponent = ({inputTitle,inputId,inputValue,setInputValue}) => {
         }
     }
 
-
- 
+    useEffect(() => {
+      if (inputTitle === 'Name') {
+        inputRef.current.focus();
+      }
+    },[])
+    const inputRef = useRef(null);
 
   return (
    
     <div className="input_element_container relative w-[100%] ">
-       <input value={inputValue} onChange={(e) => setInputValue(e.target.value)}  id={inputId}  onBlur={blurHandler} onFocus={focusHandler}  type={inputTitle === 'Password' ? 'password' : inputTitle === 'Email' ? 'email' : 'text'} className='border outline-none w-[100%] rounded-[4px]  p-[9px]' />
+       <input ref={inputRef} value={inputValue} onChange={(e) => setInputValue(e.target.value)}  id={inputId}  onBlur={blurHandler} onFocus={focusHandler}  type={inputTitle === 'Password' ? 'password' : inputTitle === 'Email' ? 'email' : 'text'} className='border outline-none w-[100%] rounded-[4px]  p-[9px]' />
        <label  htmlFor={inputId} className={`select-none input_text absolute  rounded-[2px] bg-white translate-y-[-50%] ease-in-out text-gray-500 duration-[.25s] px-[4px]  left-[15px] ${focused ? 'top-0 text-[11px]' : 'top-[50%] text-[14px]'}`}>{inputTitle} <span className='absolute right-0 bottom-[3.5px] text-red-400'>*</span> </label>
     </div>
 
