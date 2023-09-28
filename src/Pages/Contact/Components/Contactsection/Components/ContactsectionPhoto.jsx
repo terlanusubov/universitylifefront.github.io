56fa587adb5b53704a3b770cd14e213af4c7520e
HElnur <huseynove174@gmail.com>
2023-09-29 02:44:20 +0400

    Change design services , contact pages

--- src/Pages/Contact/Components/Contactsection/Components/ContactsectionPhoto.jsx
+++ src/Pages/Contact/Components/Contactsection/Components/ContactsectionPhoto.jsx
@@ -3,7 +3,7 @@ import "../../../Styles/ContactsectionPhoto.css";
 import contactBanner from '../../../Assets/download.png'
 export const ContactsectionPhoto = () => {
   return (
-    <div className="ContactsectionPhoto text-center">
+    <div className="ContactsectionPhoto text-center flex justify-center">
       <img
         src={contactBanner}
         alt=""
